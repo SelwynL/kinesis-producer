d7a55a2b8aee0179d830ce5e62a7f173a015f10f
Selwyn Lehmann <selwyn.lehmann@gmail.com>
2018-06-27 19:54:54 -0700

    produce more complex avro

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,28 +1,24 @@
 import sbt._
 
 object Dependencies {
-
   val resolutionRepos = Seq(
     "Sonatype OSS Releases"   at "http://oss.sonatype.org/content/repositories/releases/",
     "Typesafe"                at "http://repo.typesafe.com/typesafe/releases/",
     "Artima Maven Repository" at "http://repo.artima.com/releases"
   )
-
   object V {
     // Scala
     val avro                 = "1.8.2"
     val kinesisClient        = "1.9.1"
-
     // Scala (test only)
     val scalatest            = "3.0.5"
   }
-
   val Libraries = Seq(
     // Scala
-    "com.amazonaws"         %  "amazon-kinesis-client"       % V.kinesisClient,
-    "org.apache.avro"       %  "avro"                        % V.avro,
-
+    "com.amazonaws"              %  "amazon-kinesis-client"  % V.kinesisClient,
+    "com.fasterxml.jackson.core" %  "jackson-annotations"    % "2.6.7",
+    "org.apache.avro"            %  "avro"                   % V.avro,
     // Scala (test only)
-    "org.scalatest"         %% "scalatest"                   % V.scalatest   % "test"
+    "org.scalatest"              %% "scalatest"              % V.scalatest   % "test"
   )
 }
--- src/main/scala/org/selwyn/Main.scala
+++ src/main/scala/org/selwyn/Main.scala
@@ -1,11 +1,9 @@
 package org.selwyn.kproducer
 
 import org.selwyn.kproducer.codec.AvroCodec
-
 import scala.collection.JavaConverters._
 import scala.io.Source
 import java.nio.ByteBuffer
-
 import com.amazonaws.auth._
 import com.amazonaws.services.kinesis.model._
 import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration
@@ -13,26 +11,22 @@ import com.amazonaws.services.kinesis.{
   AmazonKinesis,
   AmazonKinesisClientBuilder
 }
-
 import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.avro.Schema
 import org.apache.avro.Schema.Parser
-
-case class AwsConfig(accessKey: String, secretKey: String)
 
 object Main {
 
   def main(args: Array[String]): Unit = {
     println("Running....")
 
-    val awsConfig = AwsConfig("default", "default")
-    val streamName = "logs"
-    val partitionKey = "logskey"
+    val streamName = "config-store-local"
+    val partitionKey = "config-store-local-key"
     val endpoint = "http://localhost:7000"
     val region = "us-east-1"
 
     val client: Either[Throwable, AmazonKinesis] = for {
-      provider <- getProvider(awsConfig)
+      provider <- getProvider()
       client = createKinesisClient(provider, endpoint, region)
       exists <- if (streamExists(client, streamName)) Right(true)
       else
@@ -40,21 +34,74 @@ object Main {
           new IllegalArgumentException(s"Unable to connect to '$streamName'"))
     } yield client
 
-    val avroFilename = "user.avsc"
+    // val avroFilename = "user.avsc"
+    val avroFilename = "lens_config.avsc"
+
     val schema: Schema = new Parser()
       .parse(Source.fromURL(getClass.getResource(s"/${avroFilename}")).mkString)
     val avro: AvroCodec = new AvroCodec(schema)
 
+    // val user: GenericRecord = new GenericData.Record(schema)
+    //     user.put("id", i)
+    //     user.put("name", "john dow")
+    //     user.put("email", "john.doe@gmail.com")
+
+    val lensSchema: Schema = schema.getField("lens").schema
+    val fieldSchema: Schema =
+      lensSchema.getField("fields").schema.getElementType
+    val fieldTypeSchema: Schema = fieldSchema.getField("fieldType").schema
+    val indexTypeSchema: Schema = fieldSchema.getField("indexType").schema
+    val sortOrderSchema: Schema = fieldSchema.getField("sortOrder").schema
+    val operationalStoreSchema: Schema =
+      schema.getField("operationalStore").schema
+    val storeTypeSchema: Schema =
+      operationalStoreSchema.getField("storeType").schema
+
+    val lens: GenericRecord = new GenericData.Record(lensSchema)
+    val field: GenericRecord = new GenericData.Record(fieldSchema)
+    field.put("name", "employeeId")
+    field.put("indexType",
+              new GenericData.EnumSymbol(indexTypeSchema, "LookupIndex"))
+    field.put("fieldType",
+              new GenericData.EnumSymbol(fieldTypeSchema, "StringField"))
+    field.put("description", "Employee ID")
+    field.put("required", true)
+    field.put("sortOrder",
+              new GenericData.EnumSymbol(sortOrderSchema, "IgnoreSortOrder"))
+    lens.put("name", "C1_PHONEBOOK_Employee")
+    lens.put("version", "0.1")
+    lens.put("fields", List(field).asJava)
+
+    val streamReader: Map[String, String] = Map(
+      "group.id" -> "stream-reader-local",
+      "topic.subscriptions" -> "test-employee",
+      "bootstrap.servers" -> "localhost:9093",
+      "schema.registry.url" -> "http://localhost:8081",
+      "auto.offset.reset" -> "latest"
+    )
+
+    val operationalStore: GenericRecord =
+      new GenericData.Record(operationalStoreSchema)
+    operationalStore.put("storeType",
+                         new GenericData.EnumSymbol(storeTypeSchema, "redis"))
+    operationalStore.put("region", "us-east-1")
+    operationalStore.put("connectionString", "http://localhost:6379")
+
+    val lensConfig: GenericRecord = new GenericData.Record(schema)
+    lensConfig.put("lens", lens)
+    lensConfig.put("streamReader", streamReader.asJava)
+    lensConfig.put("operationalStore", operationalStore)
+
     val complete = client.map(c => {
-
-      // PRODUCE
-      val user: GenericRecord = new GenericData.Record(schema)
-      user.put("id", 1234)
-      user.put("name", "john dow")
-      user.put("email", "john.doe@gmail.com")
-
-      val data: ByteBuffer = ByteBuffer.wrap(avro.encode(user))
-      c.putRecord(streamName, data, partitionKey)
+      val max = 1
+      for (i <- 1 to max) {
+        // PRODUCE
+        println(s"Producing to stream: ${lensConfig}")
+        //val data: ByteBuffer = ByteBuffer.wrap(avro.encode(user))
+        val data: ByteBuffer = ByteBuffer.wrap(avro.encode(lensConfig))
+        val result = c.putRecord(streamName, data, partitionKey)
+        println(s"RESULT: ${result}")
+      }
 
       // CONSUME
       // val shardId = ""
@@ -74,6 +121,7 @@ object Main {
       // }.flatten
 
       // println("RESULTS: " + result)
+      "SUCCESS"
     })
 
     println(s"COMPLETED: ${complete}")
@@ -81,8 +129,7 @@ object Main {
 
   private def serialize(string: String) = string.getBytes;
 
-  private def getProvider(
-      awsConfig: AwsConfig): Either[Throwable, AWSCredentialsProvider] = {
+  private def getProvider(): Either[Throwable, AWSCredentialsProvider] = {
     Right(new DefaultAWSCredentialsProviderChain())
   }
 
